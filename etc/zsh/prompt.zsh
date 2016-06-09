2b95731ab9d2f2be99610f25a0e7767b4ff36278
D*ck Token <dicktoken@gmail.com>
2016-06-09 21:00:57 +0200

    Changed right prompt pwd color

--- etc/zsh/prompt.zsh
+++ etc/zsh/prompt.zsh
@@ -1,5 +1,5 @@
 PROMPT=' %B%F{blue}» %f'
-RPROMPT='%B%F{black}%~ %(?.%F{green}.%F{red})%(!.#{yellow}%n.%%%F{yellow}$USERNAME)%f'
+RPROMPT='%B%F{8}%~ %(?.%F{green}.%F{red})%(!.#{yellow}%n.%%%F{yellow}$USERNAME)%f'
 # Color command correction promt
 autoload -U colors && colors
 export SPROMPT="$fg[cyan]Correct $fg[red]%R$reset_color $fg[magenta]to $fg[green]%r?$reset_color ($fg[white]YES :: NO :: ABORT :: EDIT$fg[white]): "
