f1060c0345a53874ac53deec1452cb3e26feeaa6
D*ck Token <dicktoken@gmail.com>
2016-09-01 12:51:06 +0200

    Cleaner rprompt

--- etc/zsh/prompt.zsh
+++ etc/zsh/prompt.zsh
@@ -1,7 +1,10 @@
 PROMPT=' %B%F{blue}» %f'
-RPROMPT='%B%F{8}%~ %(?.%F{green}.%F{red})%(!.#{yellow}%n.%%%F{yellow}$USERNAME)%f'
+#Old
+#RPROMPT='%B%F{8}%~ %(?.%F{green}.%F{red})%(!.#{yellow}%n.%%%F{yellow}$USERNAME)%f'
+#New
+RPROMPT='%B%F{5}%~ %(?.%F{green}.%F{red})$USERNAME%F{grey}@%F{blue}%m'
 #If in Banana Pro, or anything that uses PMU sensors; use this:
-#RPROMPT='%B%F{yellow}$(pmutemp) %B%F{5}%~ %(?.%F{green}.%F{red})%(!.%n.%%%F{yellow}$USERNAME)%f'
+#RPROMPT='%B%F{yellow}$(pmutemp) %B%F{5}%~ %(?.%F{green}.%F{red})$USERNAME%F{grey}@%F{blue}%m'
 # Color command correction promt
 autoload -U colors && colors
 export SPROMPT="$fg[cyan]Correct $fg[red]%R$reset_color $fg[magenta]to $fg[green]%r?$reset_color ($fg[white]YES :: NO :: ABORT :: EDIT$fg[white]): "
